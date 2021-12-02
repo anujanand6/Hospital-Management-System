ec7ad76a5a1f5646e66620a96e2eaf4781b41c2f
Anuj Anand <anujanand6@gmail.com>
2021-12-02 15:59:56 -0500

    Set up streamlit

--- app.py
+++ app.py
@@ -1,1 +1,31 @@
 import streamlit as st
+
+def main():
+    """
+    A Simple CRUD implementation
+    """
+    st.title("Hospital Management System")
+
+    menu = ["Home", "View Posts", "Add posts",
+            "Search", "Manage"]
+
+    choice = st.sidebar.selectbox("Menu", menu)
+
+    if choice == "Home":
+        st.subheader("Home")
+
+    elif choice == "View Posts":
+        st.subheader("View Posts")
+
+    elif choice == "Add Posts":
+        st.subheader("Add Posts")
+
+    elif choice == "Search":
+        st.subheader("Search")
+
+    elif choice == "Manage":
+        st.subheader("Manage")
+
+
+if __name__ == '__main__':
+    main()
